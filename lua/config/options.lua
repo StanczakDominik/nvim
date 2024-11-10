fd8d13464a7539397feaac26552e13382c9906c9
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2024-11-10 11:41:16 +0100

    My own config, incorporating quarto kickstart

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,74 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+vim.opt.number = true -- show linenumbers
+
+vim.opt.timeoutlen = 400 -- until which-key pops up
+vim.opt.updatetime = 250 -- for autocommands and hovers
+
+-- don't ask about existing swap files
+vim.opt.shortmess:append("A")
+
+vim.opt.showmode = false -- mode is already in statusline
+
+local tabsize = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = tabsize
+vim.opt.tabstop = tabsize
+
+-- consistent number column
+vim.opt.signcolumn = "yes:1"
+
+-- how to show autocomplete menu
+vim.opt.completeopt = "menuone,noinsert"
+
+-- global statusline
+vim.opt.laststatus = 3
+
+vim.cmd([[
+let g:currentmode={
+       \ 'n'  : '%#String# NORMAL ',
+       \ 'v'  : '%#Search# VISUAL ',
+       \ 's'  : '%#ModeMsg# VISUAL ',
+       \ "\<C-V>" : '%#Title# V·Block ',
+       \ 'V'  : '%#IncSearch# V·Line ',
+       \ 'Rv' : '%#String# V·Replace ',
+       \ 'i'  : '%#ModeMsg# INSERT ',
+       \ 'R'  : '%#Substitute# R ',
+       \ 'c'  : '%#CurSearch# Command ',
+       \ 't'  : '%#ModeMsg# TERM ',
+       \}
+]])
+
+-- hide cmdline when not used
+vim.opt.cmdheight = 1
+
+-- split right and below by default
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+--tabline
+vim.opt.showtabline = 1
+
+--windowline
+vim.opt.winbar = "%f"
+
+-- don't continue comments automagically
+-- https://neovim.io/doc/user/options.html#'formatoptions'
+vim.opt.formatoptions:remove("c")
+vim.opt.formatoptions:remove("r")
+vim.opt.formatoptions:remove("o")
+
+-- scroll before end of window
+vim.opt.scrolloff = 5
+
+-- (don't == 0) replace certain elements with prettier ones
+vim.opt.conceallevel = 0
+
+-- additional builtin vim packages
+-- filter quickfix list with Cfilter
+vim.cmd.packadd("cfilter")
+
+-- Polish and English spelling
+vim.opt.spelllang = { "pl", "en" }
---
+++ lua/plugins/activitywatch.lua
@@ -0,0 +1,3 @@
+return {
+  { "https://github.com/ActivityWatch/aw-watcher-vim"},
+}
